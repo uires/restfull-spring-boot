b943e6469a20a2c1df68110df53c53d1c11b6dd7
uires <uiresdeivide1@gmail.com>
2018-12-12 16:35:11 -0300

    Alterando implementação do repository

--- src/main/java/com/sousa/cursomc/repository/CategoriaRepository.java
+++ src/main/java/com/sousa/cursomc/repository/CategoriaRepository.java
@@ -1,10 +1,10 @@
 package com.sousa.cursomc.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.sousa.cursomc.entity.Categoria;
 
 @Repository
-public interface CategoriaRepository extends CrudRepository<Categoria, Long> {
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
