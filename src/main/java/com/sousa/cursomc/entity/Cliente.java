3398724ac4c7c1324d8a5120bc0fcfa80eacc3eb
uires <uiresdeivide1@gmail.com>
2018-12-19 17:10:35 -0300

    Adicionando duas novas entidadde e modificando enum

---
+++ src/main/java/com/sousa/cursomc/entity/Cliente.java
@@ -0,0 +1,140 @@
+package com.sousa.cursomc.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sousa.cursomc.entity.enums.TipoCliente;
+
+@Entity
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 2622791574918180424L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String email;
+	private String cpf;
+	private String cnpj;
+	private Integer tipoCliente;
+
+	private List<Endereco> endereco = new ArrayList<Endereco>();
+
+	Set<String> telefones = new HashSet<>();
+
+	public Cliente() {
+	}
+
+	public Cliente(Long id, String nome, String email, String cpf, String cnpj, TipoCliente tipoCliente) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpf = cpf;
+		this.cnpj = cnpj;
+		this.tipoCliente = tipoCliente.getCodigo();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public TipoCliente getTipoCliente() {
+		return TipoCliente.toEnum(this.tipoCliente);
+	}
+
+	public void setTipoCliente(TipoCliente tipoCliente) {
+		this.tipoCliente = tipoCliente.getCodigo();
+	}
+
+	public List<Endereco> getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(List<Endereco> endereco) {
+		this.endereco = endereco;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + ", cpf=" + cpf + ", cnpj=" + cnpj
+				+ ", tipoCliente=" + tipoCliente + "]";
+	}
+
+}
---
+++ src/main/java/com/sousa/cursomc/entity/Endereco.java
@@ -0,0 +1,160 @@
+package com.sousa.cursomc.entity;
+
+import java.io.Serializable;
+
+public class Endereco implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String rua;
+	private String bairro;
+	private String cep;
+	private String lougadouro;
+	private String numero;
+	private String complemento;
+
+	private Cliente cliente;
+	
+	private Cidade cidade;
+
+	public Endereco() {
+
+	}
+
+	public Endereco(String rua, String bairro, String cep, String lougadouro, String numero, String complemento,
+			Cliente cliente, Cidade cidade) {
+		super();
+		this.rua = rua;
+		this.bairro = bairro;
+		this.cep = cep;
+		this.lougadouro = lougadouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.cliente = cliente;
+		this.cidade = cidade;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLougadouro() {
+		return lougadouro;
+	}
+
+	public void setLougadouro(String lougadouro) {
+		this.lougadouro = lougadouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bairro == null) ? 0 : bairro.hashCode());
+		result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+		result = prime * result + ((complemento == null) ? 0 : complemento.hashCode());
+		result = prime * result + ((lougadouro == null) ? 0 : lougadouro.hashCode());
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		result = prime * result + ((rua == null) ? 0 : rua.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		if (bairro == null) {
+			if (other.bairro != null)
+				return false;
+		} else if (!bairro.equals(other.bairro))
+			return false;
+		if (cep == null) {
+			if (other.cep != null)
+				return false;
+		} else if (!cep.equals(other.cep))
+			return false;
+		if (complemento == null) {
+			if (other.complemento != null)
+				return false;
+		} else if (!complemento.equals(other.complemento))
+			return false;
+		if (lougadouro == null) {
+			if (other.lougadouro != null)
+				return false;
+		} else if (!lougadouro.equals(other.lougadouro))
+			return false;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		if (rua == null) {
+			if (other.rua != null)
+				return false;
+		} else if (!rua.equals(other.rua))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Endereco [rua=" + rua + ", bairro=" + bairro + ", cep=" + cep + ", lougadouro=" + lougadouro
+				+ ", numero=" + numero + ", complemento=" + complemento + "]";
+	}
+
+}
